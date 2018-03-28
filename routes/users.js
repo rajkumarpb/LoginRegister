f2655f5493d68ff2e976cfade4564030ad8c6cad
sandeepkaranam1 <sandeepkaranam@rocketmail.com>
2018-03-27 23:30:19 -0400

    nodejs

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,11 @@
 var express=require('express');
 var router=express.Router();
+var mongojs=require('mongojs');
+var db=mongojs('passportapp',['users']);
+var bcrypt=require('bcryptjs');
+var passport=require('passport');
+var LocalStrategy=require('passport-local').Strategy;
+
 
 //Login Page-get Request
 router.get('/login',function(req,res){
@@ -10,6 +16,51 @@ router.get('/login',function(req,res){
 router.get('/register',function(req,res){
 	res.render('register');
 });
+
+
+// Register - POST
+router.post('/register', function(req, res){
+	// Get Form Values
+	var name     		= req.body.name;
+	var email    		= req.body.email;
+	var username 		= req.body.username;
+	var password 		= req.body.password;
+	var password2 		= req.body.password2;
+
+	/*
+	// Validation
+	req.checkBody('name', 'Name field is required').notEmpty();
+	req.checkBody('email', 'Email field is required').notEmpty();
+	req.checkBody('email', 'Please use a valid email address').isEmail();
+	req.checkBody('username', 'Username field is required').notEmpty();
+	req.checkBody('password', 'Password field is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+	*/
+	// Check for errors
+	//var errors = req.validationErrors();
+
+		var newUser = {
+			name: name,
+			email: email,
+			username:username,
+			password: password
+		}
+
+		db.users.insert(newUser, function(err, doc){
+			if(err){
+				res.send(err);
+			} else {
+				console.log('User Added...');
+
+				//Success Message
+				req.flash('success', 'You are registered and can now log in');
+
+				// Redirect after register
+				res.location('/');
+				res.redirect('/');
+			}
+		});
 	
+});
 
 module.exports=router;
